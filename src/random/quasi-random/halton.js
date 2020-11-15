79814ac00b74afa33208cb2d933e357819c306a1
KM <the.real.laplace@gmail.com>
2020-11-15 22:04:22 +0200

    chore(commit): force

--- src/random/quasi-random/halton.js
+++ src/random/quasi-random/halton.js
@@ -12,12 +12,12 @@ class Halton {
 	random () {
 		const base = this._base
 
-		let reverse_base = 1 / base
 		let n = this._index
+		let reverse_base = 1 / base
 		let value = 0
 		while (n > 0) {
 			value += (n % base) * reverse_base
-			n /= base
+			n = Math.floor(n / base)
 			reverse_base /= base
 		}
 
