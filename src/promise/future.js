d20d28f0de01892cd7583a3a68249c1132b3aeba
KM <the.real.laplace@gmail.com>
2021-10-18 01:06:31 +0300

    chore(commit): force

--- src/promise/future.js
+++ src/promise/future.js
@@ -8,6 +8,18 @@ class Future {
 	}
 
 	promise () { return this._promise }
+
+	static resolve (result) {
+		const future = new Future()
+		future.resolve(result)
+		return future
+	}
+
+	static reject (reason) {
+		const future = new Future()
+		future.reject(reason)
+		return future
+	}
 }
 
 module.exports = { Future }
