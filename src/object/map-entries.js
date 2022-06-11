39f31701c37bcbaad8852f7c80f67604b17bec9c
KM <the.real.laplace@gmail.com>
2022-06-11 23:06:43 +0300

    chore(commit): force

--- src/object/map-entries.js
+++ src/object/map-entries.js
@@ -1,25 +1,28 @@
 
-const __map = (dst, src, fnMap) => {
+const __mapEntries = (dst, src, fnMap) => {
 	for (const entry of Object.entries(src)) {
 		const [ key, value ] = fnMap(entry)
 		dst[key] = value
 	}
 }
 
-const map$$$ = (obj, fnMap) => {
+const mapEntries$$$ = (obj, fnMap) => {
 	const tmp = {}
-	__map(tmp, obj, fnMap)
+	__mapEntries(tmp, obj, fnMap)
 	for (const key of Object.keys(obj)) { delete obj[key] }
 	Object.assign(obj, tmp)
 	return obj
 }
 
-const map = (obj, fnMap) => {
+const mapEntries = (obj, fnMap) => {
 	const res = {}
-	__map(res, obj, fnMap)
+	__mapEntries(res, obj, fnMap)
 	return res
 }
 
-map.$$$ = map$$$
+mapEntries.$$$ = mapEntries$$$
 
-module.exports = { map }
+module.exports = {
+	__mapEntries,
+	mapEntries,
+}
