94719427e2ff2e67b11f96fcffeb28a808e0210c
KM <the.real.laplace@gmail.com>
2022-07-30 02:05:10 +0300

    chore(commit): force

--- src/array/at.js
+++ src/array/at.js
@@ -1,6 +1,6 @@
 
 const atIndex = (length, index) => index === undefined ? 0
-	: index < 0 ? length - index
+	: index < 0 ? length + index
 	: index
 
 const at = (arr, index) => arr[atIndex(arr.length, index)]
