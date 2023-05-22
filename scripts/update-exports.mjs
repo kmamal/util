bff9d8d9548d300db324eaeacfff33629f0dc7e0
KM <the.real.laplace@gmail.com>
2023-05-22 23:09:47 +0300

    chore(commit): force

--- scripts/update-exports.mjs
+++ scripts/update-exports.mjs
@@ -30,14 +30,11 @@ const recurse = async (dirPath) => {
 			) { continue }
 
 			const filePath = `./${Path.join(dirPath, entry.name)}`
-			if (filePath === './src/index.js') {
-				pkg.main = filePath
-				continue
-			}
 
-			const key = `./${filePath.endsWith('/index.js')
-				? filePath.slice(6, -9)
-				: filePath.slice(6, -3)}`
+			const key = filePath === './src/index.js' ? '.'
+				: `./${filePath.endsWith('/index.js')
+					? filePath.slice(6, -9)
+					: filePath.slice(6, -3)}`
 			pkg.exports[key] = filePath
 		}
 	}
