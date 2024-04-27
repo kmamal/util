52915c71f2dc734ec2030a2aaf58053ab32c0312
KM <the.real.laplace@gmail.com>
2024-04-27 03:19:37 +0100

    chore(commit): force

--- src/array/partition.js
+++ src/array/partition.js
@@ -2,7 +2,7 @@ const { __partitionByHoare } = require('./sorting/quicksort/partition-by-hoare')
 
 const __partition = (arr, start, end, fnPred) => {
 	const fnCmp = (a, _) => fnPred(a) ? -1 : 1
-	__partitionByHoare(arr, start, end, null, fnCmp)
+	return __partitionByHoare(arr, start, end, null, fnCmp)
 }
 
 module.exports = { __partition }
