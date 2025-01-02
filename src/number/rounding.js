a2b866be1489ca6ca52e9d6bc74e5425671355d7
KM <the.real.laplace@gmail.com>
2025-01-02 23:20:36 +0200

    chore(commit): force

--- src/number/rounding.js
+++ src/number/rounding.js
@@ -1,10 +1,40 @@
+const { nextToward } = require('../ieee-float/double')
 
-const floorTo = (x, unit) => Math.floor(x / unit) * unit
-const ceilTo = (x, unit) => Math.ceil(x / unit) * unit
-const roundTo = (x, unit) => Math.round(x / unit) * unit
+const floorTo = (x, unit) => {
+	const signX = Math.sign(x)
+	const absX = Math.abs(x)
+	const absUnit = Math.abs(unit)
+	return signX * Math.floor(absX / absUnit) * absUnit
+}
+
+const ceilTo = (x, unit) => {
+	const signX = Math.sign(x)
+	const absX = Math.abs(x)
+	const absUnit = Math.abs(unit)
+	return signX * Math.ceil(absX / absUnit) * absUnit
+}
+
+const roundTo = (x, unit) => {
+	const signX = Math.sign(x)
+	const absX = Math.abs(x)
+	const absUnit = Math.abs(unit)
+	return signX * Math.round(absX / absUnit) * absUnit
+}
+
+const fixFloatRounding = (x, unit) => {
+	const signX = Math.sign(x)
+	const absX = Math.abs(x)
+	const absUnit = Math.abs(unit)
+	const mod = absX % absUnit
+	if (mod === 0) { return x }
+	const direction = mod > absUnit - mod ? Infinity : -Infinity
+	return signX * nextToward(absX, direction)
+}
+
 
 module.exports = {
 	floorTo,
 	ceilTo,
 	roundTo,
+	fixFloatRounding,
 }
