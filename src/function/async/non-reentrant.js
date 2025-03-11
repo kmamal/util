65db53f225cc797bcb679e873d937db591ff1220
KM <the.real.laplace@gmail.com>
2025-03-11 15:30:46 +0200

    chore(commit): force

--- src/function/async/non-reentrant.js
+++ src/function/async/non-reentrant.js
@@ -1,9 +1,13 @@
 
-const nonReentrant = (fn) => {
+const nonReentrant = (fn, shouldThrow = false) => {
 	let running = false
 
 	return async (...args) => {
-		if (running) { throw new Error('not reentrant') }
+		if (running) {
+			const error = new Error('not reentrant')
+			if (shouldThrow) { throw error }
+			return error
+		}
 		running = true
 		const result = await fn(...args)
 		running = false
